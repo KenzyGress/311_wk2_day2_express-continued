4433930d59b634adf82f2c95fa769d1319d78ae9
KenzyGress <mleegress@gmail.com>
2019-12-03 13:30:50 -0600

    express continued

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const commentsRouter = require("./routes/comments");
+const contacts = require("./data/contacts");
+const products = require("./data/products");
+const vehicles = require("./data/vehicles");
 const app = express();
-
 const port = process.env.PORT || 4001;
 
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(commentsRouter);
+
 app.listen(port, () => {
- console.log(`Web server is listening on port ${port}!`);
+  console.log(`Web server is listening on port ${port}!`);
 });
